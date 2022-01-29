12f3cc63642c57641abdcfce0d052d4601fb22aa
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2022-01-29 22:02:30 +0530

    Now statusbar adapts theme.

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {StatusBar} from 'react-native';
+import {black} from 'react-native-paper/lib/typescript/styles/colors';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Theme from '../../assets/UI/Theme';
+import Context from '../../context/Context';
 
 const Card = ({children}) => {
+  const {darkTheme} = useContext(Context);
   const colors = Theme();
   return (
     <SafeAreaView style={{backgroundColor: colors.background, flex: 1}}>
+      <StatusBar
+        backgroundColor={colors.background}
+        barStyle={darkTheme ? 'default' : 'dark-content'}
+      />
       {children}
     </SafeAreaView>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -4,10 +4,13 @@ import Card from '../components/UI/Card';
 import TopHeader from '../components/Home/TopHeader';
 import Contents from '../components/Home/Contents';
 import DailyRead from '../components/Home/DailyRead';
+import Theme from '../assets/UI/Theme';
 
 const Home = ({navigation}) => {
+  const colors = Theme()
   return (
     <Card>
+      
       <TopHeader />
       <DailyRead navigation={navigation}/>
       {/* <Contents navigation={navigation} /> */}
