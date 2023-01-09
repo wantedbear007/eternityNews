f5f77d4065fa1e8c9da88d31d3dff5b2ba67c896
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2023-01-09 19:08:53 +0530

    Beta changes

--- components/Home/DailyRead.js
+++ components/Home/DailyRead.js
@@ -1,151 +1,292 @@
-import React, {useEffect, useState} from 'react';
-import {
-  View,
-  FlatList,
-  StyleSheet,
-  ToastAndroid,
-  RefreshControl,
-} from 'react-native';
-import axios from 'axios';
+/***
+ Use this component inside your React Native Application.
+ A scrollable list with different item type
+ */
+import React, {Component} from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
+import Sample from './Sample';
 import DailyReadRender from './DailyReadRender';
-import TrendingNews from './TrendingNews';
-import AsyncStorage from '@react-native-community/async-storage';
-import SkeletonHome from './SkeletonHome';
-import ErrorScreen from '../UI/ErrorScreen';
+import Theme from '../../assets/UI/Theme';
 
-const DailyRead = ({navigation, colors}) => {
-  const [newsQuantity, setNewsQuantity] = useState(15);
-  const [news, setNews] = useState([]);
-  const [newsEnd, setNewsEnd] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [offSet, setOffSet] = useState(0);
-  const [errorStatus, setErrorStatus] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
-  // delete
-  const [del, setDel] = useState([]);
-
-  // const storeData = async val => {
-  //   try {
-  //     const jsonVal = JSON.stringify(val);
-  //     await AsyncStorage.setItem('newsData', jsonVal);
-  //   } catch (e) {}
-  // };
-
-  // const getStoredData = async () => {
-  //   try {
-  //     const fetchedNewsData = await AsyncStorage.getItem('newsData');
-  //     const fetchedJSON = JSON.parse(fetchedNewsData);
-  //     setNews(fetchedJSON);
-  //     setLoading(false);
-  //   } catch (e) {}
-  // };
-
-  async function fetchData() {
-    try {
-      await axios
-        .get(
-          `https://inshorts.me/news/all?offset=${offSet}&limit=${newsQuantity}`,
-        )
-
-        .then(response => {
-          setNews(response.data.data.articles);
-          // console.log(response);
-          // storeData(response.data.data.articles)
-          setLoading(false);
-        })
-        .catch(err => {
-          if (err.response) {
-            setErrorStatus(true);
-          }
-        });
-    } catch (error) {}
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [newsQuantity]);
-
-  const renderItems = ({item}) => {
-    if (loading) {
-      return <SkeletonHome />;
-    } else {
-      return (
-        <DailyReadRender
-          offSetNumber={offSet}
-          colors={colors}
-          item={item}
-          navigation={navigation}
-        />
-      );
-    }
-  };
-
-  // Infinite Scrolling
-  const infiniteScrolling = () => {
-    if (newsQuantity >= 300) {
-      ToastAndroid.show('No more news', ToastAndroid.SHORT);
-      setNewsEnd(true);
-    } else {
-      setOffSet(newsQuantity);
-      setNewsQuantity(newsQuantity + 10);
-    }
-  };
-
-  const wait = timeout => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
-  };
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-    fetchData();
-  }, []);
-  return (
-    <View style={styles.compactContainer}>
-      {errorStatus ? (
-        <>
-          <ErrorScreen colors={colors} />
-        </>
-      ) : (
-        <FlatList
-          scrollEventThrottle={16}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
-          data={news}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={renderItems}
-          onEndReached={infiniteScrolling}
-          ListHeaderComponent={
-            <TrendingNews
-              colors={colors}
-              refresh={refreshing}
-              navigation={navigation}
-            />
-          }
-          ListFooterComponent={!newsEnd && <SkeletonHome />}
-          legacyImplementation={false}
-          showsVerticalScrollIndicator={true}
-          maxToRenderPerBatch={5}
-          initialNumToRender={5}
-          onEndReachedThreshold={0.5}
-          viewabilityConfig={{
-            minimumViewTime: 300,
-            viewAreaCoveragePercentThreshold: 100,
-            waitForInteraction: true,
-          }}
-          // removeClippedSubviews={true}
-        />
-      )}
-    </View>
-  );
+const ViewTypes = {
+  FULL: 0,
+  HALF_LEFT: 1,
+  HALF_RIGHT: 2,
 };
 
-export default DailyRead;
+let containerCount = 0;
 
-const styles = StyleSheet.create({
-  compactContainer: {
-    marginHorizontal: 10,
+/***
+ * To test out just copy this component and render in you root component
+ */
+export default class RecycleTestComponent extends React.Component {
+  constructor(args) {
+    super(args);
+    // console.log(Sample)
+    let {width} = Dimensions.get('window');
+    const handleTheme = () => {
+      const colors = Theme();
+
+      return colors;
+    };
+
+    // console.log()
+    //Create the data provider and provide method which takes in two rows of data and return if those two are different or not.
+    //THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
+    let dataProvider = new DataProvider((r1, r2) => {
+      return r1 !== r2;
+    });
+
+    //Create the layout provider
+    //First method: Given an index return the type of item e.g ListItemType1, ListItemType2 in case you have variety of items in your list/grid
+    //Second: Given a type and object set the exact height and width for that type on given object, if you're using non deterministic rendering provide close estimates
+    //If you need data based check you can access your data provider here
+    //You'll need data in most cases, we don't provide it by default to enable things like data virtualization in the future
+    //NOTE: For complex lists LayoutProvider will also be complex it would then make sense to move it to a different file
+    this._layoutProvider = new LayoutProvider(
+      index => {
+        return ViewTypes.FULL;
+      },
+
+      (type, dim) => {
+        switch (type) {
+          case ViewTypes.FULL:
+            dim.width = width;
+            dim.height = 140;
+            break;
+          default:
+            dim.width = 0;
+            dim.height = 0;
+        }
+      },
+    );
+
+    this._rowRenderer = this._rowRenderer.bind(this);
+
+    this.state = {
+      dataProvider: dataProvider.cloneWithRows(Sample),
+    };
+  }
+
+  _rowRenderer(type, data) {
+    return (
+      <DailyReadRender
+        offSetNumber={10}
+        colors={this.props.colors}
+        item={data}
+        navigation={this.props.navigation}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <RecyclerListView
+        layoutProvider={this._layoutProvider}
+        dataProvider={this.state.dataProvider}
+        rowRenderer={this._rowRenderer}
+        
+      />
+    );
+  }
+}
+const styles = {
+  container: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#00a1f1',
   },
-});
+  containerGridLeft: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#ffbb00',
+  },
+  containerGridRight: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#7cbb00',
+  },
+};
+
+// import React, {useEffect, useState} from 'react';
+// import {
+//   View,
+//   FlatList,
+//   StyleSheet,
+//   ToastAndroid,
+//   RefreshControl,
+//   Text,
+// } from 'react-native';
+// import axios from 'axios';
+// import DailyReadRender from './DailyReadRender';
+// import TrendingNews from './TrendingNews';
+// import AsyncStorage from '@react-native-community/async-storage';
+// import SkeletonHome from './SkeletonHome';
+// import ErrorScreen from '../UI/ErrorScreen';
+// import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
+
+// const DailyRead = ({navigation, colors}) => {
+//   const [newsQuantity, setNewsQuantity] = useState(15);
+//   const [news, setNews] = useState([]);
+//   const [newsEnd, setNewsEnd] = useState(false);
+//   const [loading, setLoading] = useState(true);
+//   const [offSet, setOffSet] = useState(0);
+//   const [errorStatus, setErrorStatus] = useState(false);
+//   const [refreshing, setRefreshing] = useState(false);
+
+//   // delete
+//   const [del, setDel] = useState([]);
+
+//   // const storeData = async val => {
+//   //   try {
+//   //     const jsonVal = JSON.stringify(val);
+//   //     await AsyncStorage.setItem('newsData', jsonVal);
+//   //   } catch (e) {}
+//   // };
+
+//   // const getStoredData = async () => {
+//   //   try {
+//   //     const fetchedNewsData = await AsyncStorage.getItem('newsData');
+//   //     const fetchedJSON = JSON.parse(fetchedNewsData);
+//   //     setNews(fetchedJSON);
+//   //     setLoading(false);
+//   //   } catch (e) {}
+//   // };
+
+//   async function fetchData() {
+//     try {
+//       await axios
+//         .get(
+//           `https://inshorts.me/news/all?offset=${offSet}&limit=${newsQuantity}`,
+//         )
+
+//         .then(response => {
+//           setNews(response.data.data.articles);
+//           // console.log(response);
+//           // storeData(response.data.data.articles)
+//           setLoading(false);
+//         })
+//         .catch(err => {
+//           if (err.response) {
+//             setErrorStatus(true);
+//           }
+//         });
+//     } catch (error) {}
+//   }
+
+//   useEffect(() => {
+//     fetchData();
+//   }, [newsQuantity]);
+
+//   const renderItems = ({item}) => {
+//     if (loading) {
+//       return <SkeletonHome />;
+//     } else {
+//       return (
+//         <DailyReadRender
+//           offSetNumber={offSet}
+//           colors={colors}
+//           item={item}
+//           navigation={navigation}
+//         />
+//       );
+//     }
+//   };
+
+//   // Infinite Scrolling
+//   const infiniteScrolling = () => {
+//     if (newsQuantity >= 300) {
+//       ToastAndroid.show('No more news', ToastAndroid.SHORT);
+//       setNewsEnd(true);
+//     } else {
+//       setOffSet(newsQuantity);
+//       setNewsQuantity(newsQuantity + 10);
+//     }
+//   };
+
+//   const wait = timeout => {
+//     return new Promise(resolve => setTimeout(resolve, timeout));
+//   };
+
+//   const onRefresh = React.useCallback(() => {
+//     setRefreshing(true);
+//     wait(2000).then(() => setRefreshing(false));
+//     fetchData();
+//   }, []);
+
+// // trial area
+// const LolFooter = () => {
+//   return (
+//     <Text>Footer hai</Text>
+//   )
+// }
+
+// const Magic = () => {
+//   return (
+//     <Text>Hello world</Text>
+//   )
+// }
+
+//   return (
+//     <View style={styles.compactContainer}>
+//       {errorStatus ? (
+//         <>
+//           <ErrorScreen colors={colors} />
+//         </>
+//       ) : (
+//         <View>
+//           <Text>Heello world</Text>
+//           <RecyclerListView
+//             // style={{flex: 1}}
+//             contentContainerStyle={{margin: 3}}
+//             renderFooter={LolFooter}
+//             // dataProvider={news}
+//             layoutProvider={Magic}
+//           />
+//         </View>
+//         /* <FlatList
+//           scrollEventThrottle={16}
+//           refreshControl={
+//             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+//           }
+//           data={news}
+//           keyExtractor={(item, index) => index.toString()}
+//           renderItem={renderItems}
+//           onEndReached={infiniteScrolling}
+//           ListHeaderComponent={
+//             <TrendingNews
+//               colors={colors}
+//               refresh={refreshing}
+//               navigation={navigation}
+//             />
+//           }
+//           ListFooterComponent={!newsEnd && <SkeletonHome />}
+//           legacyImplementation={false}
+//           showsVerticalScrollIndicator={true}
+//           maxToRenderPerBatch={5}
+//           initialNumToRender={5}
+//           onEndReachedThreshold={0.5}
+//           viewabilityConfig={{
+//             minimumViewTime: 300,
+//             viewAreaCoveragePercentThreshold: 100,
+//             waitForInteraction: true,
+//           }}
+//           // removeClippedSubviews={true}
+//         /> */
+//       )}
+//     </View>
+//   );
+// };
+
+// export default DailyRead;
+
+// const styles = StyleSheet.create({
+//   compactContainer: {
+//     flex: 1,
+//     marginHorizontal: 10,
+//   },
+// });
---
+++ components/Home/Sample.js
@@ -0,0 +1,276 @@
+const Sample = [
+  {
+    hashId: 'xcfjdivk-1',
+    oldHashId:
+      'attempts-made-to-portray-particular-community-as-extremists-kerala-minister-1673199663744',
+    authorName: 'Mohammad Anab',
+    content:
+      'Kerala minister PA Mohamed Riyas said that attempts are being made in the country to portray a particular community as "extremists". Riyas\' remarks referred to a cultural programme organised at a Kerala school where a particular community was allegedly depicted as extremists. "In this particular issue, the alleged Sangh Parivar [links] of the persons concerned should be investigated," he added.',
+    sourceName: 'Press Trust of India',
+    sourceUrl:
+      'https://www.ptinews.com/news/national/attempts-in-country-to-portray-particular-community-as-extremists-claims-kerala-minister/490760.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      'Attempts made to portray particular community as extremists: Kerala minister',
+    subtitle: 'Riyas is CPI(M) MLA from Beypore constituency',
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673197992549_823.jpg?',
+    shortenedUrl: 'https://shrts.in/UeaJn',
+    categoryNames: ['politics', 'national'],
+    relevancyTags: ['politics', 'national'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673199663000,
+  },
+  {
+    hashId: 'b3ssetjt-1',
+    oldHashId:
+      'boat-carrying-185-rohingya-refugees-lands-in-indonesias-aceh-1673199453020',
+    authorName: 'Mayukh Debnath',
+    content:
+      "A boat carrying 185 Rohingya refugees, including a pregnant woman, arrived on the shores of Indonesia's Aceh province on Sunday. The refugees appeared generally in good condition, but four of them were sick and are being treated by doctors, a police official said. More than 500 Rohingya refugees have reached Indonesia in the past several weeks. \n",
+    sourceName: 'Reuters',
+    sourceUrl:
+      'https://www.reuters.com/world/asia-pacific/boat-with-185-rohingya-refugees-land-indonesias-aceh-2023-01-08/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title: "Boat carrying 185 Rohingya refugees lands in Indonesia's Aceh",
+    subtitle: 'The boat landed at the Kuala Gigieng beach',
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673198052941_965.jpg?',
+    shortenedUrl: 'https://shrts.in/AMx9g',
+    categoryNames: ['world'],
+    relevancyTags: ['world'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673199453000,
+  },
+  {
+    hashId: 'btlzd4rx-1',
+    oldHashId:
+      'nadal-is-going-to-be-an-absolute-animal-at-aus-open-2023-wilander-1673199202201',
+    authorName: 'Anant Kaur',
+    content:
+      'Defending champion Rafael Nadal is going to be an "absolute animal" at upcoming Australian Open 2023, said seven-time Grand Slam champion Mats Wilander. "He just has to get there healthy...I\'m sure that he\'s going to play his best tennis," he added. "He has a big chance to win the Australian Open, as he does [at] the French Open," Wilander stated.',
+    sourceName: 'Sportskeeda',
+    sourceUrl:
+      'https://www.sportskeeda.com/amp/tennis/news-rafael-nadal-will-animal-australian-open-2023-predicts-mats-wilander-highlights-separates-spaniard-roger-federer?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title: 'Nadal is going to be an absolute animal at Aus Open 2023: Wilander',
+    subtitle: 'Australian Open will begin on January 16',
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673196811896_373.jpg?',
+    shortenedUrl: 'https://shrts.in/f7oxc',
+    categoryNames: ['sports'],
+    relevancyTags: ['sports'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673199202000,
+  },
+  {
+    hashId: 'qascbfs6-1',
+    oldHashId:
+      'kashmir-village-receives-electricity-for-first-time-after-75-years-1673199127116',
+    authorName: 'Aditya Nair',
+    content:
+      'A tribal area in south Kashmir\'s Anantnag with about 200 people received an electricity connection after almost 75 years. The people of Tethan village so far relied on traditional wood for their energy needs and used lamps and candlelight. "Our children will study under light now...Our problems have been solved now. We are grateful to the government," said a resident. ',
+    sourceName: 'ANI News',
+    sourceUrl:
+      'https://aninews.in/news/national/general-news/electricity-reaches-kashmir-village-after-75-years-elated-locals-thank-govt20230108204538/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title: 'Kashmir village receives electricity for first time after 75 years',
+    subtitle: "'Previous generations couldn't see this miracle'",
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673196022923_713.jpg?',
+    shortenedUrl: 'https://shrts.in/7t29C',
+    categoryNames: ['national'],
+    relevancyTags: ['national'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673199127000,
+  },
+  {
+    hashId: 'zxw1ys7y-1',
+    oldHashId:
+      'kalvin-phillips-learnt-a-good-lesson-on-keeping-fit-man-city-manager-1673198976284',
+    authorName: 'Aditya Nair',
+    content:
+      'Manchester City manager Pep Guardiola said midfielder Kalvin Phillips has "improved his level" but must be in perfect condition all year. "It will maybe be a good lesson for him in the future...you have to be ready because this level is so demanding," he said. Guardiola had criticised Phillips for being overweight after leaving him out of a match squad.',
+    sourceName: 'Reuters',
+    sourceUrl:
+      'https://www.reuters.com/lifestyle/sports/citys-phillips-learns-lesson-keeping-fit-guardiola-2023-01-08/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      'Kalvin Phillips learnt a good lesson on keeping fit: Man City manager',
+    subtitle: "'For sure he will play in the League Cup'",
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673195186975_636.jpg?',
+    shortenedUrl: 'https://shrts.in/9VunK',
+    categoryNames: ['sports'],
+    relevancyTags: ['football', 'premier_league'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673198976000,
+  },
+  {
+    hashId: 'lqwy31ou-1',
+    oldHashId:
+      'questions-on-congs-pm-candidate-are-distraction-attempts-rahul-gandhi-1673198776358',
+    authorName: 'Aditya Nair',
+    content:
+      'Rahul Gandhi dismissed questions over the Congress\' Prime Ministerial candidate by calling them "distraction attempts". "There is uproar on social media regarding the Bharat Jodo Yatra, but the national media doesn\'t show anything. Distraction attempts are made by asking who will be the CM or PM," he said. Gandhi added that the government "doesn\'t think" about the employment of people.',
+    sourceName: 'Hindustan Times',
+    sourceUrl:
+      'https://www.hindustantimes.com/cities/others/questions-of-congress-pm-candidate-are-distraction-attempts-rahul-gandhi-101673189600964-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      "Questions on Cong's PM candidate are distraction attempts: Rahul Gandhi",
+    subtitle: "'This nation is lying to its youth'",
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673193178416_311.jpg?',
+    shortenedUrl: 'https://shrts.in/o4ytz',
+    categoryNames: ['politics'],
+    relevancyTags: ['politics'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673198776000,
+  },
+  {
+    hashId: 'q5k8rlns-1',
+    oldHashId:
+      'indiaus-trade-policy-forum-to-meet-in-washington-on-january-11-1673198671604',
+    authorName: 'Anisha Joneja',
+    content:
+      'The India-US Trade Policy Forum (TPF) will hold a meeting in Washington on January 11, the Ministry of Commerce and Industry announced. The meeting will be co-chaired by Commerce and Industry Minister Piyush Goyal and US Trade Representative Katherine Tai. The last TPF meeting was held on November 23, 2021. Goyal will be on a three-day official visit to US.',
+    sourceName: 'Moneycontrol',
+    sourceUrl:
+      'https://www.moneycontrol.com/news/trade-2/indo-us-trade-policy-forum-to-meet-on-january-11-in-washington-9828551.html/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title: 'India-US Trade Policy Forum to meet in Washington on January 11',
+    subtitle: 'India-US TPF was established in 2005',
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673197149101_322.jpg?',
+    shortenedUrl: 'https://shrts.in/iHkMC',
+    categoryNames: ['business'],
+    relevancyTags: ['business'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673198671000,
+  },
+  {
+    hashId: 'rsdmj0xp-1',
+    oldHashId:
+      'i-recently-rejected-a-big-film-i-never-run-after-money-diljit-dosanjh-1673198669556',
+    authorName: 'Anmol Sharma',
+    content:
+      'Actor-singer Diljit Dosanjh, in a recent interview, said he never runs after money. He added, "If I had cared about money, I would\'ve done more films, more marriage shows, 4-5 Punjabi films every year." He stated, "I recently rejected a big film. I don\'t do something just for the sake of money."',
+    sourceName: 'Hindustan Times',
+    sourceUrl:
+      'https://www.hindustantimes.com/entertainment/bollywood/interview-diljit-dosanjh-i-don-t-work-for-money-i-rejected-a-big-film-just-yesterday-101673147137337-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      'I recently rejected a big film, I never run after money: Diljit Dosanjh',
+    subtitle: "'I am doing everything at my own pace'",
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673198024611_162.jpg?',
+    shortenedUrl: 'https://shrts.in/ffKEe',
+    categoryNames: ['entertainment'],
+    relevancyTags: ['celebrities'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 800,
+    important: false,
+    createdAt: 1673198669000,
+  },
+  {
+    hashId: 'opefk0mb-1',
+    oldHashId:
+      'did-demonetisation-respect-the-tapasya-of-poor-certainly-not-rahul-1673198581820',
+    authorName: 'Mohammad Anab',
+    content:
+      'Congress leader Rahul Gandhi said that the demonetisation move by the BJP-led Centre in November 2016 was an "attack" on "tapasya" of the economically "poor" population of India. "Did demonetisation respect the \'tapasya\' of the poor? Certainly not," Rahul said. He added, "Bharat Jodo Yatra is successful because not only Congress or one individual...but lakhs are also doing \'tapasya\'."',
+    sourceName: 'The Print',
+    sourceUrl:
+      'https://theprint.in/india/pm-modi-wants-everyone-in-country-should-worship-him-congs-focus-is-tapasya-rahul-gandhi/1304795/?amp=&utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      "Did demonetisation respect the 'tapasya' of poor? Certainly not: Rahul",
+    subtitle: "RSS had got 'pooja' done from Britishers: Rahul",
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673196909306_244.jpg?',
+    shortenedUrl: 'https://shrts.in/F4x3p',
+    categoryNames: ['politics', 'national'],
+    relevancyTags: ['politics', 'national'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 600,
+    important: false,
+    createdAt: 1673198581000,
+  },
+  {
+    hashId: 'hcqmt1kx-1',
+    oldHashId:
+      'court-orders-bihar-police-to-lodge-rape-case-against-ias-officer-exmla-1673198575941',
+    authorName: 'Shreyasi Banerjee',
+    content:
+      'A court in Bihar on Sunday ordered police to register a rape case against senior IAS officer Sanjeev Hans and former RJD MLA Gulab Yadav in connection with the alleged rape of a woman at a hotel in Delhi in 2021. The duo has been accused of gang-raping her at gunpoint. The MLA also allegedly made a video of her.',
+    sourceName: 'The Print',
+    sourceUrl:
+      'https://theprint.in/india/court-orders-patna-police-to-register-rape-case-against-ias-officer-former-rjd-mla/1304560/?amp=&utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+    title:
+      'Court orders Bihar Police to lodge rape case against IAS officer, ex-MLA',
+    subtitle: 'The MLA reportedly blackmailed the survivor',
+    imageUrl:
+      'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2023/01_jan/8_sun/img_1673196813590_378.jpg?',
+    shortenedUrl: 'https://shrts.in/RkHgZ',
+    categoryNames: ['national'],
+    relevancyTags: ['national'],
+    language: 'ENGLISH',
+    countryCode: 'IN',
+    score: 800,
+    important: false,
+    createdAt: 1673198575000,
+  },
+];
+
+export default Sample
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
